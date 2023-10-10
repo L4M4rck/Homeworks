bb266a1b363fcf14ca8a158ebba340584d5f5150
L4M4rck <88438168+L4M4rck@users.noreply.github.com>
2023-10-10 11:30:04 -0500

    tercer challenge

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AddCategory } from './components/AddCategory'
+import { GifGrid } from './components/GifGrid'
 import FirstApp from './FirstApp'
+import { GiftExpertApp } from './GiftExpertApp'
 import { Suma } from './Suma'
 
 
@@ -9,9 +12,8 @@ import { Suma } from './Suma'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <FirstApp title= "My First App" />
-    <Suma />
+    <GiftExpertApp />
+
 
   </React.StrictMode>,
 )
