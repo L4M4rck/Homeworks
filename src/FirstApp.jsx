6a4eaf9e452fab674b621675e62782c99ce4dfd5
L4M4rck <88438168+L4M4rck@users.noreply.github.com>
2023-10-10 11:24:48 -0500

    segundo challenge boton

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,12 +1,22 @@
-const title = 'First App'
+import PropTypes from 'prop-types';
 
-function FirstApp(){
+const FirstApp = ( { title, sum } ) =>{
     return (
     <>
     <h1> { title } </h1>
-    <span> 10 </span>
+    <span> { sum } </span>
     </>
     )
   }
-  
+
+FirstApp.propTypes = {
+  title: PropTypes.string.isRequired,
+  sum: PropTypes.number.isRequired
+}
+
+FirstApp.defaultProps = {
+  title: 'No hay titulo',
+  sum: 300
+}
+
   export default FirstApp
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import FirstApp from './FirstApp'
+import { Suma } from './Suma'
+
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <FirstApp />
+    <FirstApp title= "My First App" />
+    <Suma />
+
   </React.StrictMode>,
 )
