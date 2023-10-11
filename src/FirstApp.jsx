45e84349efd64b7e31524905ade8a75f76033b57
L4M4rck <88438168+L4M4rck@users.noreply.github.com>
2023-10-10 21:39:28 -0500

    challenge impar par

--- src/FirstApp.jsx
+++ src/FirstApp.jsx
@@ -1,5 +1,37 @@
 const title = 'First App'
 
+const verificarParImpar = function(numero) {
+  if (typeof numero !== 'number') {
+    console.error('Error: El valor proporcionado no es un número.');
+    return;
+  }
+
+  if (numero % 2 === 0) {
+    console.log(`${numero} es un número par.`);
+  } else {
+    console.log(`${numero} es un número impar.`);
+  }
+}
+
+verificarParImpar(10);
+
+/* Arrow Function
+const verificarParImpar = (numero) => {
+  if (typeof numero !== 'number') {
+    console.error('Error: El valor proporcionado no es un número.');
+    return;
+  }
+
+  if (numero % 2 === 0) {
+    console.log(`${numero} es un número par.`);
+  } else {
+    console.log(`${numero} es un número impar.`);
+  }
+};
+
+verificarParImpar(7);
+*/ 
+
 function FirstApp(){
     return (
     <>
