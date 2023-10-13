535b33eb769b2e2c5bfc686a649c4d4df5362198
L4M4rck <88438168+L4M4rck@users.noreply.github.com>
2023-10-12 21:01:31 -0500

    challenge 8

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Counter from "./counter"
+import { MultipleCustomHook } from "./components/MultipleCustomHooks"
 import "./styles.css"
 
 export default function App() {
   return (
     <div className="App">
-      <Counter value={0} />
+      <MultipleCustomHook />
     </div>
   )
 }
